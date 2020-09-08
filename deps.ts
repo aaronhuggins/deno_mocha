0f9542c15979789d0f2bdb4faf3fde6c530255df
Aaron Huggins <ahuggins@nethealthshops.com>
2020-09-08 11:26:39 -0500

    Set versions

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // @deno-types="https://unpkg.com/@types/mocha@8.0.3/index.d.ts"
 export * from 'https://unpkg.com/mocha@8.1.3/mocha.js'
 export type { Mocha, BrowserMocha } from 'https://unpkg.com/@types/mocha@8.0.3/index.d.ts'
-export { parse } from 'https://deno.land/std/flags/mod.ts'
-export { walk } from 'https://deno.land/std/fs/walk.ts'
-export { globToRegExp } from 'https://deno.land/std/path/glob.ts'
-export { join, normalize } from 'https://deno.land/std/path/mod.ts'
+export { parse } from 'https://deno.land/std@0.68.0/flags/mod.ts'
+export { walk } from 'https://deno.land/std@0.68.0/fs/walk.ts'
+export { globToRegExp } from 'https://deno.land/std@0.68.0/path/glob.ts'
+export { join, normalize } from 'https://deno.land/std@0.68.0/path/mod.ts'
